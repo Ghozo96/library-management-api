dfbe617a7ea8e3d8e29997a9b8720661e65c86e6
Ghazala <ghazalamarwan@gmail.com>
2023-10-30 20:30:42 +0200

    Added husky

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
 import express, { Request, Response } from 'express';
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
-    res.send("Hello world!")
-})
+  res.send('Hello world!');
+});
 
 export default app;
-
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
 import dotenv from 'dotenv';
 
-import app from './app'
+import app from './app';
 
 dotenv.config();
 
-const SERVER_PORT = process.env.SERVER_PORT ?? 3000
+const SERVER_PORT = process.env.SERVER_PORT ?? 3000;
 
 const server = app.listen(SERVER_PORT, () => {
-    console.log("Server is running on port", SERVER_PORT)
-})
+  console.log('Server is running on port', SERVER_PORT);
+});
 
 export default server;
